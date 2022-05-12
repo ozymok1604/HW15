416ed7649c660f3b4f28afe9cfb915ac7430bff1
ozymok1604 <ozimok1604ua@gmail.com>
2022-05-12 13:47:35 +0300

    HW15

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Book } from "./Contacts/Contacts";
+
+function App() {
+  return (
+    <div className="App">
+      <Book />
+    </div>
+  );
+}
+
+export default App;
